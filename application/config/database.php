8577ff792b54d6d10e28d365a32b44c5bf651a0b
Ade Ramadhana Pratama <171111020@mhs.stiki.ac.id>
2021-01-24 17:42:16 +0700

    news api
    add news & get news

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => '',
-	'password' => '',
-	'database' => '',
+	'hostname' => 'tomboati.bgskr-project.my.id',
+	'username' => 'tomboati',
+	'password' => '1sampaitombo',
+	'database' => 'tomboati',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
--- application/controllers/TestApi.php
+++ application/controllers/TestApi.php
@@ -1,14 +1,11 @@
 <?php
 
-// import library dari REST_Controller
-require APPPATH . 'libraries/REST_Controller.php';
-
-// extends class dari REST_Controller
-class TestApi extends REST_Controller{
+class TestApi extends CI_Controller{
 
   // constructor
   public function __construct(){
     parent::__construct();
+    $this->load->database();
   }
 
   public function index_get(){
@@ -24,7 +21,7 @@ class TestApi extends REST_Controller{
   }
 
   public function user_get(){
-
+    $response=[];
     // testing response
     $response['status']=200;
     $response['error']=false;
@@ -35,9 +32,58 @@ class TestApi extends REST_Controller{
     $response['user']['detail']['specialize']='Android,IOS,WEB,Desktop';
 
     //tampilkan response
-    $this->response($response);
-
+    
+    $this->throw(200, $response);
+    return;
   }
+
+  public function add_news(){ 
+    $response = [];
+    
+    $id_news_info = $this->input->post('id_news_info'); 
+    $judul_news   = $this->input->post('judul_news');
+    $deskripsi    = $this->input->post('deskripsi');
+    $konten       = $this->input->post('konten');
+    $tanggal      = date('Y-m-d H:i:s');
+
+    $data = array(
+      'IDNEWSINFO'      => $id_news_info,
+      'JUDULNEWS'       => $judul_news,
+      'DESKRIPSINEWS'   => $deskripsi,
+      'CONTENTNEWS'      => $konten,
+      'TANGGALNEWS'     => $tanggal  
+    );
+
+    $this->db->insert('NEWS_INFO', $data);
+    if($this->db->affected_rows()>0){
+      $response['error']    = false;
+      $response['messages'] = 'Berhasil input';
+      $this->throw(200, $response);
+      return;
+    }
+    $response['error']    = true;
+    $response['messages'] = 'GAgal input';
+    $this->throw(200, $response);
+    return;
+  }
+
+  public function get_news(){
+    $response = [];
+
+    $data = $this->db->get('NEWS_INFO')->result();
+
+    $response['error']    = false;
+    $response['messages'] = 'Berhasil input';
+    $response['data']     = $data;
+    $this->throw(200, $response);
+    return;
+  }
+
+  private function throw($statusCode, $response){
+    $this->output->set_status_header($statusCode)
+    ->set_content_type('application/json')
+    ->set_output(json_encode($response));
+}
 
 }
 
