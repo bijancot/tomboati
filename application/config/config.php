cb4ed42dd2a8e68695b2187c73e317bd81520175
Ade Ramadhana Pratama <171111020@mhs.stiki.ac.id>
2021-01-24 16:02:36 +0700

    test library rest
    test library rest
    remove index.php

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'https://tomboati.bgskr-project.my.id/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
