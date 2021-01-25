6423e82dc79c9de4c479e4ad9b7ef3aff8e61d3d
Ade Ramadhana Pratama <171111020@mhs.stiki.ac.id>
2021-01-25 20:55:01 +0700

    login register paket api
    post login api
    post register api
    get paket api

---
+++ application/controllers/api/PaketApi.php
@@ -0,0 +1,46 @@
+<?php
+
+class PaketApi extends CI_Controller{
+
+    // constructor
+    public function __construct(){
+        parent::__construct();
+        $this->load->database();
+    }
+
+    // paket get
+    public function paket_get(){
+        $response = [];
+
+        $month          = date('m');
+        $monthFilter    = $this->input->post('monthFilter');
+
+        if($monthFilter !=null){
+            $data = $this->db->query('SELECT * FROM PAKET WHERE MONTH(TANGGALKEBERANGKATAN) = "'.$monthFilter.'"')->result();
+    
+            $response['error']    = false;
+            $response['messages'] = 'Sukses Tampil Data';
+            $response['data']     = $data;
+            $this->throw(200, $response);
+            return;
+        }else{
+            $data = $this->db->query('SELECT * FROM PAKET WHERE MONTH(TANGGALKEBERANGKATAN) = "'.$month.'"')->result();
+    
+            $response['error']    = false;
+            $response['messages'] = 'Sukses Tampil Data';
+            $response['data']     = $data;
+            $this->throw(200, $response);
+            return;
+        }
+
+        
+    }
+
+    private function throw($statusCode, $response){
+        $this->output->set_status_header($statusCode)
+        ->set_content_type('application/json')
+        ->set_output(json_encode($response));
+    }
+
+}
+?>
