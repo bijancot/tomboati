79392b1c10c8f1621abb48b98422d51bd5887b7f
DedyHrmawan <dedyhermawan076@gmail.com>
2021-01-29 19:48:22 +0700

    Menyesuaikan News dan menambah aksi edit delete

--- application/views/news/VEditNews.php
+++ application/views/news/VEditNews.php
@@ -22,59 +22,61 @@
                 <div class="row align-items-center justify-content-between">
                     <div class="col-auto mt-4">
                         <h1 class="page-header-title">
-                            <div class="page-header-icon"><i data-feather="activity"></i></div>
+                            <a href="<?= base_url(); ?>news">
+                                <button class="btn btn-blue btn-icon mr-2 my-1" type="button"><i class="fas fa-arrow-left"></i></button>
+                            </a>
                             News
                         </h1>
-                        Daftar News
+
                     </div>
                 </div>
             </div>
         </div>
     </header>
     <div class="container mt-n10">
-        <div class="card mb-4">
-            <div class="card-header">
-                <a href="<?= site_url('news/tambahNews'); ?>" class='btn btn-primary btn-sm' type='submit'>Tambah News</a>
-            </div>
-            <div class="card-body">
-                <div class="datatable">
-                    <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
-                        <thead>
-                            <tr>
-                                <th>No</th>
-                                <th>Judul News</th>
-                                <th>Deskripsi News</th>
-                                <th>Isi News</th>
-                                <th>Tanggal</th>
-                                <th>Aksi</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <td>1</td>
-                                <td>Judul</td>
-                                <td>Deskripsi</td>
-                                <td>Isi</td>
-                                <td>2011/04/25</td>
-                                <td>
-                                    <a href="<?= site_url('News/editNews'); ?>" class="btn btn-datatable btn-icon btn-yellow mr-2"><i data-feather="edit-2"></i></a>
-                                    <a class="btn btn-datatable btn-icon btn-red mr-2"><i data-feather="trash-2"></i></a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+        <div class="row">
+            <div class="col-lg-9">
+                <!-- Default Bootstrap Form Controls-->
+                <div id="default">
+                    <div class="card mb-4">
+                        <div class="card-header">Form Edit News</div>
+                        <div class="card-body">
+                            <!-- Component Preview-->
+                            <div class="sbp-preview">
+                                <div class="sbp-preview-content">
+                                    <form>
+                                        <div class="form-group">
+                                            <label for="JudulNews">Judul News</label>
+                                            <input class="form-control" id="JudulNews" type="text" />
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="DeskripsiNews">Deskripsi News</label>
+                                            <textarea class="form-control" id="DeskripsiNews" rows="3"></textarea>
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="IsiNews">Isi News</label>
+                                            <textarea class="form-control" id="IsiNews" rows="15"></textarea>
+                                        </div>
+                                    </form>
+                                    <div class="text-md-right">
+                                        <button type="submit" class="btn btn-primary "> Submit </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-
+    
     <script src="<?= base_url(); ?>assets/js/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
     <script src="<?= base_url(); ?>assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     <script src="<?= base_url(); ?>assets/js/scripts.js"></script>
     <script src="<?= base_url(); ?>assets/js/plugin/datatables/jquery.dataTables.min.js" crossorigin="anonymous"></script>
     <script src="<?= base_url(); ?>assets/js/plugin/datatables/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
     <script src="<?= base_url(); ?>assets/js/demo/datatables-demo.js"></script>
-
+   
 </body>
 
 </html>
