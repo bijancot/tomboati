60cd93225aea5a1218254f6d097ba8b516f25efe
DedyHrmawan <dedyhermawan076@gmail.com>
2021-01-31 20:50:20 +0700

    News

--- application/views/news/VNews.php
+++ application/views/news/VNews.php
@@ -1,20 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content />
-    <meta name="author" content />
-    <link href="<?= base_url(); ?>assets/css/styles.css" rel="stylesheet" />
-    <link href="<?= base_url(); ?>assets/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
-    <link rel="icon" type="image/x-icon" href="<?= base_url(); ?>assets/img/logo_tomboati.png" />
-    <script data-search-pseudo-elements defer src="<?= base_url(); ?>assets/js/plugin/font-awesome/5.13.0/js/all.min.js" crossorigin="anonymous"></script>
-    <script src="<?= base_url(); ?>assets/js/plugin/feather-icons/4.27.0/feather.min.js" crossorigin="anonymous"></script>
-
-</head>
-
 <body>
     <header class="page-header page-header-dark bg-gradient-primary-to-secondary pb-10">
         <div class="container">
@@ -25,7 +11,7 @@
                             <div class="page-header-icon"><i data-feather="activity"></i></div>
                             News
                         </h1>
-                        Daftar News
+                        <div class="page-header-subtitle">Daftar News</div>
                     </div>
                 </div>
             </div>
@@ -61,20 +47,32 @@
                                     <a class="btn btn-datatable btn-icon btn-red mr-2"><i data-feather="trash-2"></i></a>
                                 </td>
                             </tr>
+                            <!-- <?php foreach ($news_info as $row) : ?>
+                                <tr>
+                                    <td width>
+                                    <?php echo $row->judulnews ?>
+                                    </td>
+                                    <td>
+                                        <?php echo $row->deskripsinews ?>
+                                    </td>
+                                    <td>
+                                    <?php echo $row->contentnew ?>
+                                    </td>
+                                    <td>
+                                    <?php echo $row->tanggalnews ?>
+                                    </td>
+                                    <td>
+                                       <a href="<?= site_url('News/editNews'); ?>" class="btn btn-datatable btn-icon btn-yellow mr-2"><i data-feather="edit-2"></i></a>
+                                       <a href="<?= site_url('News/hapusNews'); ?>"class="btn btn-datatable btn-icon btn-red mr-2"><i data-feather="trash-2"></i></a>
+                                 </td>
+                                </tr>
+                            <?php endforeach; ?> -->
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
     </div>
-
-    <script src="<?= base_url(); ?>assets/js/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
-    <script src="<?= base_url(); ?>assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="<?= base_url(); ?>assets/js/scripts.js"></script>
-    <script src="<?= base_url(); ?>assets/js/plugin/datatables/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-    <script src="<?= base_url(); ?>assets/js/plugin/datatables/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-    <script src="<?= base_url(); ?>assets/js/demo/datatables-demo.js"></script>
-
 </body>
 
 </html>
