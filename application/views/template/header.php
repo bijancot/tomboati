3b3312081e9bdad97a2611beb92359fc9ead57b5
DedyHrmawan <dedyhermawan076@gmail.com>
2021-01-31 20:46:35 +0700

    fix bug footer & ubah icon menu

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,19 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-    <meta name="description" content />
-    <meta name="author" content />
-    <link href="<?= base_url(); ?>assets/css/styles.css" rel="stylesheet" />
-    <link href="<?= base_url(); ?>assets/css/dataTables.bootstrap4.min.css" rel="stylesheet" crossorigin="anonymous" />
-    <link rel="icon" type="image/x-icon" href="<?= base_url(); ?>assets/img/logo_tomboati.png" />
-    <script src="<?= base_url(); ?>assets/js/plugin/feather-icons/4.27.0/feather.min.js" crossorigin="anonymous"></script>
-    
-</head>
-
 <body>
     <nav class="topnav navbar navbar-expand shadow navbar-light bg-white" id="sidenavAccordion">
         <img class="navbar-brand-img ml-3" src="<?= base_url(); ?>assets/img/logo_tomboati.png"></img>
@@ -86,13 +73,7 @@
         </ul>
     </nav>
     
-    <script src="<?= base_url(); ?>assets/js/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
-    <script src="<?= base_url(); ?>assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    <script src="<?= base_url(); ?>assets/js/scripts.js"></script>
-    <script src="<?= base_url(); ?>assets/js/plugin/datatables/jquery.dataTables.min.js" crossorigin="anonymous"></script>
-    <script src="<?= base_url(); ?>assets/js/plugin/datatables/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
-    <script src="<?= base_url(); ?>assets/js/demo/datatables-demo.js"></script>
-   
+    <?php $this->load->view("template/script.php") ?>
 </body>
 
 </html>
---
+++ application/views/template/script.php
@@ -0,0 +1,6 @@
+<script src="<?= base_url(); ?>assets/js/jquery-3.5.1.min.js" crossorigin="anonymous"></script>
+<script src="<?= base_url(); ?>assets/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+<script src="<?= base_url(); ?>assets/js/scripts.js"></script>
+<script src="<?= base_url(); ?>assets/js/plugin/datatables/jquery.dataTables.min.js" crossorigin="anonymous"></script>
+<script src="<?= base_url(); ?>assets/js/plugin/datatables/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+<script src="<?= base_url(); ?>assets/js/demo/datatables-demo.js"></script>
