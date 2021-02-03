79e059a95088d1abcc5c9476820bea52229cbaf2
Ade Ramadhana Pratama <171111020@mhs.stiki.ac.id>
2021-02-03 18:39:42 +0700

    chat view
    chat page list view
    detail chat view

--- application/controllers/Chat.php
+++ application/controllers/Chat.php
@@ -7,11 +7,34 @@ class Chat extends CI_Controller
         $this->load->helper('form');
         $this->load->helper('url');
         $this->load->library('table');
+        $this->load->model('MChat');
     }
+
     public function index()
     {
-        $data = array('title' => 'Chat | Tombo Ati');
+        $dataChat = $this->MChat->getChat();
+
+        $data = array(
+            'title'     => 'Chat | Tombo Ati',
+            'chat'      => $dataChat
+        );
+
         $this->template->load('template/template', 'chat/VChat', $data);
+        $this->load->view("template/script.php");
+    }
+
+    public function detailChat($idChatRoom)
+    {
+        $detailDataChat = $this->MChat->getDetailChat($idChatRoom);
+        $chatFrom       = $this->MChat->getChatFrom($idChatRoom);
+
+        $data = array(
+            'title'     => 'Detail Chat | Tombo Ati',
+            'chat'      => $detailDataChat,
+            'chatFrom'  => $chatFrom
+        );
+
+        $this->template->load('template/template', 'chat/DetailChat', $data);
         $this->load->view("template/script.php");
     }
 }
