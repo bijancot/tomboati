e44b435c16e177c8290aacdcf0639536802ad9e1
DedyHrmawan <dedyhermawan076@gmail.com>
2021-02-11 19:27:10 +0700

    Fix bug Datatable

--- application/views/template/script.php
+++ application/views/template/script.php
@@ -3,5 +3,7 @@
 <script src="<?= base_url(); ?>assets/js/scripts.js"></script>
 <script src="<?= base_url(); ?>assets/js/plugin/datatables/jquery.dataTables.min.js" crossorigin="anonymous"></script>
 <script src="<?= base_url(); ?>assets/js/plugin/datatables/dataTables.bootstrap4.min.js" crossorigin="anonymous"></script>
+<script src="<?= base_url(); ?>assets/js/demo/datatables-demo.js"></script>
+
 <!-- datepicker -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/js/bootstrap-datepicker.min.js"></script>
