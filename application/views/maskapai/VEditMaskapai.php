f61c9c8163d5a60a49e656436e80a4d9340185de
DedyHrmawan <dedyhermawan076@gmail.com>
2021-02-18 17:32:38 +0700

    Kasih button back

--- application/views/maskapai/VEditMaskapai.php
+++ application/views/maskapai/VEditMaskapai.php
@@ -5,10 +5,10 @@
                 <div class="row align-items-center justify-content-between">
                     <div class="col-auto mt-4">
                         <h1 class="page-header-title">
-                        <!-- <a href="<?= base_url(); ?>umroh">
-                            <button class="btn btn-blue btn-icon mr-2 my-1" type="button"><i class="fas fa-arrow-left"></i></button>
-                        </a> -->
-                        Maskapai
+                            <a>
+                                <button class="btn btn-yellow btn-icon mr-2 my-1" type="button" onclick="history.back(-1)"><i class="fas fa-arrow-left"></i></button>
+                            </a>
+                            Edit Maskapai
                         </h1>
                     </div>
                 </div>
@@ -28,20 +28,20 @@
                                 <div class="sbp-preview-content">
                                     <?php foreach ($maskapai as $row) {
                                     ?>
-                                    <?= form_open_multipart('maskapai/aksiEditMaskapai/'.$row['IDMASKAPAI']) ?>
-                                    <div class="form-group">
-                                        <label for="namaMaskapai">Nama Maskapai</label>
-                                        <input class="form-control" name="namaMaskapai" id="namaMaskapai" value="<?= $row['NAMAMASKAPAI']; ?>" type="text"  required="" />
-                                    </div>
-                                    <div class="form-group">
-                                        <label for="image-source">Gambar Maskapai</label>
-                                        <!-- wadah preview -->
-                                        <img id="image-preview-edit" src="<?= $row['IMAGEMASKAPAI']; ?>" alt="image preview"/>
-                                        <div class="custom-file">
-                                            <input type="file" value="<?= $row['IMAGEMASKAPAI']; ?>" name="imageMaskapai" class="custom-file-input" id="image-source" onchange="previewImage();">
-                                            <label class="custom-file-label" for="image-source">Upload Gambar</label>
+                                        <?= form_open_multipart('maskapai/aksiEditMaskapai/' . $row['IDMASKAPAI']) ?>
+                                        <div class="form-group">
+                                            <label for="namaMaskapai">Nama Maskapai</label>
+                                            <input class="form-control" name="namaMaskapai" id="namaMaskapai" value="<?= $row['NAMAMASKAPAI']; ?>" type="text" required="" />
                                         </div>
-                                    </div>
+                                        <div class="form-group">
+                                            <label for="image-source">Gambar Maskapai</label>
+                                            <!-- wadah preview -->
+                                            <img id="image-preview-edit" src="<?= $row['IMAGEMASKAPAI']; ?>" alt="image preview" />
+                                            <div class="custom-file">
+                                                <input type="file" value="<?= $row['IMAGEMASKAPAI']; ?>" name="imageMaskapai" class="custom-file-input" id="image-source" onchange="previewImage();">
+                                                <label class="custom-file-label" for="image-source">Upload Gambar</label>
+                                            </div>
+                                        </div>
                                     <?php
                                     }
                                     ?>
@@ -58,22 +58,20 @@
     </div>
 </body>
 <script type="text/javascript">
-
-//preview sebelum upload
-function previewImage() {
-document.getElementById("image-preview-edit").style.display = "block";
-var oFReader = new FileReader();
-oFReader.readAsDataURL(document.getElementById("image-source").files[0]);
-oFReader.onload = function(oFREvent) {
-document.getElementById("image-preview-edit").src = oFREvent.target.result;
-};
-};
-// Add the following code if you want the name of the file appear on select
-$(".custom-file-input").on("change", function() {
-var fileName = $(this).val().split("\\").pop();
-$(this).siblings(".custom-file-label").addClass("selected").html(fileName);
-});
-
+    //preview sebelum upload
+    function previewImage() {
+        document.getElementById("image-preview-edit").style.display = "block";
+        var oFReader = new FileReader();
+        oFReader.readAsDataURL(document.getElementById("image-source").files[0]);
+        oFReader.onload = function(oFREvent) {
+            document.getElementById("image-preview-edit").src = oFREvent.target.result;
+        };
+    };
+    // Add the following code if you want the name of the file appear on select
+    $(".custom-file-input").on("change", function() {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".custom-file-label").addClass("selected").html(fileName);
+    });
 </script>
 <script>
     Pusher.logToConsole = true;
