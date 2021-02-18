09a8367b6499a49564424ef66e79130b3478f306
DedyHrmawan <dedyhermawan076@gmail.com>
2021-02-18 17:33:25 +0700

    Tambah view change password

---
+++ application/views/admin/VChangePassword.php
@@ -0,0 +1,47 @@
+<header class="page-header page-header-dark bg-gradient-primary-to-secondary pb-10">
+    <div class="container">
+        <div class="page-header-content pt-4">
+            <div class="row align-items-center justify-content-between">
+                <div class="col-auto mt-4">
+                    <h1 class="page-header-title">
+                        <a>
+                            <button class="btn btn-yellow btn-icon mr-2 my-1" type="button" onclick="history.back(-1)"><i class="fas fa-arrow-left"></i></button>
+                        </a>
+                        <div class="page-header-icon"><i data-feather="key"></i></div>
+                        Change Password
+                    </h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<div class="container mt-n10">
+    <hr class="mt-0 mb-4" />
+    <div class="row">
+        <div class="col-lg-8">
+            <div class="card mb-4">
+                <div class="card-header">Change Password</div>
+                <div class="card-body">
+                    <form>
+                        <!-- Form Group (current password)-->
+                        <!-- <div class="form-group">
+                            <label class="small mb-1" for="currentPassword">Current Password</label>
+                            <input class="form-control" id="currentPassword" type="password" placeholder="Masukan Password Sekarang" />
+                        </div> -->
+                        <!-- Form Group (new password)-->
+                        <div class="form-group">
+                            <label class="small mb-1" for="newPassword">New Password</label>
+                            <input class="form-control" id="newPassword" type="password" placeholder="Masukan Password Baru" />
+                        </div>
+                        <!-- Form Group (confirm password)-->
+                        <div class="form-group">
+                            <label class="small mb-1" for="confirmPassword">Confirm Password</label>
+                            <input class="form-control" id="confirmPassword" type="password" placeholder="Konfirmasi Password Baru" />
+                        </div>
+                        <button class="btn btn-primary" type="button">Simpan</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
