9f998c3f89793ab8a93180bbbd1b8d4208c7d85a
DedyHrmawan <dedyhermawan076@gmail.com>
2021-02-19 19:53:11 +0700

    test

--- application/views/admin/VDashboard.php
+++ application/views/admin/VDashboard.php
@@ -9,7 +9,7 @@
                     <div class="col-auto mt-4">
                         <h1 class="page-header-title">
                             <div class="page-header-icon"><i data-feather="globe"></i></div>
-                            Dashboard
+                            Dashboardd
                         </h1>
                     </div>
                 </div>
