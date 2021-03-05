fe5f281a2ea14ad59eeb957939df071f2dd6615d
DedyHrmawan <dedyhermawan076@gmail.com>
2021-03-05 13:22:06 +0700

    Update form pengumuman

--- application/controllers/Pengumuman.php
+++ application/controllers/Pengumuman.php
@@ -4,16 +4,24 @@ class Pengumuman extends CI_Controller
     public function __construct()
     {
         parent::__construct();
+        $this->load->helper('form');
+        $this->load->helper('file');
+        $this->load->library('table');
+        $this->load->library('upload');
         date_default_timezone_set('Asia/Jakarta');
         $this->load->model('MNotifikasi');
     }
 
     public function index()
     {
-        $countMessage   = $this->MNotifikasi->countMessage();
+
+        //notifikasi
+        $countMessage    = $this->MNotifikasi->countMessage();
         $dataNotifChat   = $this->MNotifikasi->dataNotifChat();
         $countJamaahDaftar      = $this->MNotifikasi->countJamaahDaftar();
 
+        // echo $this->db->last_query();
+        //parse
         $data = array(
             'title' => 'Pengumuman | Tombo Ati',
             'countMessage' => $countMessage,
--- application/views/pengumuman/VPengumuman.php
+++ application/views/pengumuman/VPengumuman.php
@@ -1,18 +1,99 @@
-<header class="page-header page-header-dark bg-gradient-primary-to-secondary pb-10">
-    <div class="container-fluid">
-        <div class="page-header-content pt-4">
-            <div class="row align-items-center justify-content-between">
-                <div class="col-auto mt-4">
-                    <h1 class="page-header-title">
-                        <a>
-                            <button class="btn btn-yellow btn-icon mr-2 my-1" type="button" onclick="history.back(-1)"><i class="fas fa-arrow-left"></i></button>
-                        </a>
-                        <div class="page-header-icon"><i data-feather="briefcase"></i></div>
-                        <?= $title; ?>
-                    </h1>
-                    Pengumuman
+<style>
+    .ck-editor__editable {
+        max-height: 100%;
+        min-height: 300px;
+    }
+</style>
+<body>
+    <header class="page-header page-header-dark bg-gradient-primary-to-secondary pb-10">
+        <div class="container-fluid">
+            <div class="page-header-content pt-4">
+                <div class="row align-items-center justify-content-between">
+                    <div class="col-auto mt-4">
+                        <h1 class="page-header-title">
+                            <div class="page-header-icon"><i class="fas fa-bullhorn ml-2 fa-xs"></i></div>
+                            <?= $title; ?>
+                        </h1>
+                        Pengumuman
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <div class="container-fluid mt-n10">
+        <div class="row">
+            <div class="col-lg-12">
+                <!-- Default Bootstrap Form Controls-->
+                <div id="default">
+                    <div class="card mb-4">
+                        <div class="card-header">Pengumuman</div>
+                        <div class="card-body">
+                            <!-- Component Preview-->
+                            <div class="sbp-preview">
+                                <div class="sbp-preview-content">
+                                    <div class="form-group">
+                                        <label for="judulPengumuman">Judul</label>
+                                        <input class="form-control" name="judulPengumuman" id="judulPengumuman" type="text" required />
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="contentPengumuman">Deskripsi</label>
+                                        <textarea name="contentPengumuman" class="form-control" id="contentPengumuman" rows="3"></textarea>
+                                    </div>
+                                    <div class="text-md-right">
+                                        <button type="submit" class="btn btn-primary "> Submit </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</header>
+</body>
+<script>
+    ClassicEditor
+        .create(document.querySelector('#contentPengumuman'))
+        .then(editor => {
+            console.log(editor);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+</script>
+<script type="text/javascript">
+    //preview sebelum upload
+    function previewFoto() {
+        document.getElementById("foto-preview").style.display = "block";
+        var oFReader = new FileReader();
+        oFReader.readAsDataURL(document.getElementById("source-foto").files[0]);
+        oFReader.onload = function(oFREvent) {
+            document.getElementById("foto-preview").src = oFREvent.target.result;
+        };
+    };
+    // Add the following code if you want the name of the file appear on select
+    $(".foto").on("change", function() {
+        var fileName = $(this).val().split("\\").pop();
+        $(this).siblings(".label-foto").addClass("selected").html(fileName);
+    });
+</script>
+<script>
+    Pusher.logToConsole = true;
+    var pusher = new Pusher('ee692ab95bb9aeaa1dcc', {
+        cluster: 'ap1',
+        forceTLS: true
+    });
+    var channel = pusher.subscribe('my-channel');
+    channel.bind('my-event', function(response) {
+        xhr = $.ajax({
+            method: 'POST',
+            url: "<?php echo base_url() ?>/Notifikasi/listNotifikasi",
+            success: function(response) {
+                $('.list-notifikasi').html(response);
+            }
+        })
+    });
+    $('.list-notifikasi').on('click', '.notifikasi', function(e) {
+        console.log("Clicked");
+    });
+</script>
