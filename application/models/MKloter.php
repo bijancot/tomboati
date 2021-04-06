a6b59788e04ff41fe752c322819ecfaf4dd06230
Ronald Arrival Fajar <38059376+juraganpisang@users.noreply.github.com>
2021-04-06 19:25:31 +0700

    [UPD] Kloter Aman

--- application/models/MKloter.php
+++ application/models/MKloter.php
@@ -17,17 +17,10 @@ class MKloter extends CI_Model
         return $query->result();
     }
 
-    // TAMPILAN AWAL DI KLOTER (COUNT)
-    // public function countKloterbyPaket()
-    // {
-
-    //     $query = $this->db->query("SELECT count(TRANSAKSI.IDPAKET) as jumlah FROM `TRANSAKSI` JOIN `PAKET` ON `TRANSAKSI`.`IDPAKET` = `PAKET`.`IDPAKET` JOIN `PENDAFTARAN` ON `TRANSAKSI`.`KODEPENDAFTARAN` = `PENDAFTARAN`.`KODEPENDAFTARAN` WHERE `STATUSPENDAFTARAN` = 1 GROUP BY `TRANSAKSI`.IDPAKET");
-    //     return $query->result_array();
-    // }
 
     // ini untuk menampilkan di atur kloter
-    public function getSelectKloterbyPaket($IDPAKET){
-       
+    public function getSelectPaket($IDPAKET){
+
         $this->db->select('*, count(TRANSAKSI.IDPAKET) as jumlah');
         $this->db->from('TRANSAKSI');
         $this->db->join('PAKET', 'TRANSAKSI.IDPAKET = PAKET.IDPAKET');
@@ -40,39 +33,18 @@ class MKloter extends CI_Model
         return $query->result_array();
     }
 
-    // ini untuk menampilkan di atur kloter (count)
-    // public function countSelectKloterbyPaket($IDPAKET){
-       
-    //     $this->db->select('count(TRANSAKSI.IDPAKET) as jumlah');
-    //     $this->db->from('TRANSAKSI');
-    //     $this->db->join('PAKET', 'TRANSAKSI.IDPAKET = PAKET.IDPAKET');
-    //     $this->db->join('PENDAFTARAN', 'TRANSAKSI.KODEPENDAFTARAN = PENDAFTARAN.KODEPENDAFTARAN');
-    //     $this->db->where('TRANSAKSI.IDPAKET', $IDPAKET);
-    //     $this->db->where('STATUSPENDAFTARAN', 1);
-    //     $this->db->group_by('TRANSAKSI.IDPAKET');
-    //     $query = $this->db->get();
-
-    //     return $query->result_array();
-    // }
 
     // INI UNTUK PEMILIHAN KAROMAH
-    public function getSelectKloterbyJamaah($IDPAKET){
-       
-        $this->db->select('*');
-        $this->db->from('TRANSAKSI');
-        $this->db->join('PAKET', 'TRANSAKSI.IDPAKET = PAKET.IDPAKET');
-        $this->db->join('PENDAFTARAN', 'TRANSAKSI.KODEPENDAFTARAN = PENDAFTARAN.KODEPENDAFTARAN');
-        $this->db->where('TRANSAKSI.IDPAKET', $IDPAKET);
-        $this->db->where('STATUSPENDAFTARAN', 1);
-        $this->db->where('KLOTER !=', NULL);
-        $query = $this->db->get();
+    public function getKaromah($kloter){
+
+        $query = $this->db->query("SELECT * FROM `TRANSAKSI` JOIN `PAKET` ON `TRANSAKSI`.`IDPAKET` = `PAKET`.`IDPAKET` JOIN `PENDAFTARAN` ON `TRANSAKSI`.`KODEPENDAFTARAN` = `PENDAFTARAN`.`KODEPENDAFTARAN` WHERE `TRANSAKSI`.`IDPAKET` = LEFT('".$kloter."',4) AND `STATUSPENDAFTARAN` = 1 AND `KLOTER` = '".$kloter."'");
 
         return $query->result_array();
     }
 
     // INI UNTUK JAMAAH YANG SUDAH MEMPUNYAI KLOTER
-    public function getSelectKloterbyKloter($IDPAKET){
-       
+    public function getKloter($IDPAKET){
+
         $this->db->select('*');
         $this->db->from('TRANSAKSI');
         $this->db->join('PAKET', 'TRANSAKSI.IDPAKET = PAKET.IDPAKET');
@@ -87,8 +59,8 @@ class MKloter extends CI_Model
     }
 
     // INI UNTUK JAMAAH YANG BELUM MEMPUNYAI KLOTER
-    public function getSelectKloterbyUnknownKloter($IDPAKET){
-       
+    public function getUnknownKloter($IDPAKET){
+
         $this->db->select('*');
         $this->db->from('TRANSAKSI');
         $this->db->join('PAKET', 'TRANSAKSI.IDPAKET = PAKET.IDPAKET');
@@ -103,7 +75,7 @@ class MKloter extends CI_Model
 
     // INI UNTUK JAMAAH YANG SUDAH MEMPUNYAI KLOTER
     public function getSelectKloter($IDPAKET, $KLOTER){
-       
+
         $this->db->select('*');
         $this->db->from('TRANSAKSI');
         $this->db->join('PAKET', 'TRANSAKSI.IDPAKET = PAKET.IDPAKET');
@@ -117,8 +89,8 @@ class MKloter extends CI_Model
     }
 
     public function getEditKloter($IDPAKET, $KLOTER){
-       
-        $query = $this->db->query("SELECT * FROM `TRANSAKSI` JOIN `PAKET` ON `TRANSAKSI`.`IDPAKET` = `PAKET`.`IDPAKET` JOIN `PENDAFTARAN` ON `TRANSAKSI`.`KODEPENDAFTARAN` = `PENDAFTARAN`.`KODEPENDAFTARAN` WHERE `TRANSAKSI`.`IDPAKET` = '0020' AND `STATUSPENDAFTARAN` = 1 AND (`KLOTER` = '0020-UMR-VIP-A1' OR `KLOTER` IS NULL)");
+
+        $query = $this->db->query("SELECT * FROM `TRANSAKSI` JOIN `PAKET` ON `TRANSAKSI`.`IDPAKET` = `PAKET`.`IDPAKET` JOIN `PENDAFTARAN` ON `TRANSAKSI`.`KODEPENDAFTARAN` = `PENDAFTARAN`.`KODEPENDAFTARAN` WHERE `TRANSAKSI`.`IDPAKET` = ".$IDPAKET." AND `STATUSPENDAFTARAN` = 1 AND `KLOTER` = '".$KLOTER."'");
 
         return $query->result_array();
     }
@@ -137,6 +109,14 @@ class MKloter extends CI_Model
         $this->db->update('PENDAFTARAN');
     }
 
+    public function deleteJamaahKloter($kodePendaftaran)
+    {
+        $this->db->set('KLOTER', NULL);
+        $this->db->set('ISKAROMAH', NULL);
+        $this->db->where('KODEPENDAFTARAN', $kodePendaftaran);
+        $this->db->update('PENDAFTARAN');
+    }
+
     public function addKaromah($data)
     {
         $this->db->set('ISKAROMAH', 1);
@@ -144,6 +124,17 @@ class MKloter extends CI_Model
         $this->db->update('PENDAFTARAN');
     }
 
+    public function deleteKaromah($data)
+    {
+        $this->db->query("UPDATE PENDAFTARAN SET ISKAROMAH = NULL WHERE KODEPENDAFTARAN != '".$data['KODEPENDAFTARAN']."' AND KLOTER = '".$data['KLOTER']."'");
+    }
+
+    public function checkKaromah($kloter){
+        $query = $this->db->query("SELECT count(PENDAFTARAN.KODEPENDAFTARAN) AS JUMLAH FROM PENDAFTARAN WHERE `PENDAFTARAN`.`KLOTER` = '".$kloter."' AND PENDAFTARAN.ISKAROMAH != 0");
+
+        return $query->result_array();
+    }
+    // Selalu ada
     public function updateJamaah()
     {
         $this->db->set('ISSEEN', 1);
