4d6ee54c5ecdb602846d5b939f6ef8c8783c4249
DedyHrmawan <dedyhermawan076@gmail.com>
2021-09-03 11:54:31 +0700

    update api riwayat transaksi

--- application/controllers/api/Transaksi.php
+++ application/controllers/api/Transaksi.php
@@ -14,6 +14,7 @@ class Transaksi extends CI_Controller{
         
         $data = $this->db->query('SELECT TRANSAKSI.*, PAKET.NAMAPAKET FROM PENDAFTARAN JOIN TRANSAKSI ON TRANSAKSI.KODEPENDAFTARAN = PENDAFTARAN.KODEPENDAFTARAN 
         JOIN PAKET ON PAKET.IDPAKET = TRANSAKSI.IDPAKET
+        JOIN PEMBAYARAN ON PEMBAYARAN.IDTRANSAKSI = TRANSAKSI.IDTRANSAKSI
         WHERE IDUSERREGISTER = "'.$idUserRegister.'" AND STATUSTRANSAKSI = 1')->result();
 
         if($this->db->affected_rows()>0){
@@ -31,6 +32,42 @@ class Transaksi extends CI_Controller{
         
         $data = $this->db->query('SELECT TRANSAKSI.*, WISATA_HALAL.NAMAWISATA FROM PENDAFTARAN JOIN TRANSAKSI ON TRANSAKSI.KODEPENDAFTARAN = PENDAFTARAN.KODEPENDAFTARAN 
         JOIN WISATA_HALAL ON WISATA_HALAL.IDWISATAHALAL = TRANSAKSI.IDWISATAHALAL
+        WHERE IDUSERREGISTER = "'.$idUserRegister.'" AND STATUSTRANSAKSI = 1')->result();
+
+        if($this->db->affected_rows()>0){
+            $response['error']      = false;
+            $response['message']    = 'Berhasil Tampil Data';
+            $response["data"]       = $data;
+            $this->throw(200, $response);
+            return;
+        }
+    }
+
+    //riwayat transaksi umroh haji
+    public function riwayatUmrohHaji(){ 
+        $idUserRegister        = $this->input->get('idUserRegister');
+        
+        $data = $this->db->query('SELECT TRANSAKSI.*, PAKET.NAMAPAKET FROM PENDAFTARAN JOIN TRANSAKSI ON TRANSAKSI.KODEPENDAFTARAN = PENDAFTARAN.KODEPENDAFTARAN 
+        JOIN PAKET ON PAKET.IDPAKET = TRANSAKSI.IDPAKET
+        JOIN PEMBAYARAN ON PEMBAYARAN.IDTRANSAKSI = TRANSAKSI.IDTRANSAKSI
+        WHERE IDUSERREGISTER = "'.$idUserRegister.'" AND STATUSTRANSAKSI = 1')->result();
+
+        if($this->db->affected_rows()>0){
+            $response['error']      = false;
+            $response['message']    = 'Berhasil Tampil Data';
+            $response["data"]       = $data;
+            $this->throw(200, $response);
+            return;
+        }
+    }
+
+    //riwayat transaksi umroh haji
+    public function riwayatWisataHalal(){ 
+        $idUserRegister        = $this->input->get('idUserRegister');
+        
+        $data = $this->db->query('SELECT TRANSAKSI.*, PAKET.NAMAPAKET FROM PENDAFTARAN JOIN TRANSAKSI ON TRANSAKSI.KODEPENDAFTARAN = PENDAFTARAN.KODEPENDAFTARAN 
+        JOIN PAKET ON PAKET.IDPAKET = TRANSAKSI.IDPAKET
+        JOIN PEMBAYARAN ON PEMBAYARAN.IDTRANSAKSI = TRANSAKSI.IDTRANSAKSI
         WHERE IDUSERREGISTER = "'.$idUserRegister.'" AND STATUSTRANSAKSI = 1')->result();
 
         if($this->db->affected_rows()>0){
