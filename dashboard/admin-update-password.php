f6b48f60973673e0d31655edd3f62313c7799a7c
DedyHrmawan <dedyhermawan076@gmail.com>
2021-09-22 18:27:47 +0700

    update profil to db tomboati

--- dashboard/admin-update-password.php
+++ dashboard/admin-update-password.php
@@ -1,5 +1,6 @@
 <?php
 include('config.php');
+include('config2.php');
 include('fungsi.php');
 
 if(isset($_POST['update-password'])){
@@ -10,8 +11,10 @@ $password1 = $_POST['password1'];
 $enc_password1 = md5 ($password1);
 
 $query = mysqli_query($koneksi, "UPDATE mebers SET passw='$password1', passenc='$enc_password1' WHERE userid='$username' ")or die(mysql_error());
+$query2 = mysqli_query($koneksi2, "UPDATE USER_REGISTER SET PASSWORD='$password1' WHERE USERNAME='$username' ");
+
 }
-if ($query){
+if ($query && $query2){
 header('location:admin-profile-edit.php?userid='.$username.'&error=3');	
 } else {
 	echo "Perubahan Password Gagal";
