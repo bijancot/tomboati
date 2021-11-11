37c6495adb29f5ce8cbada029bae64da05832578
DedyHrmawan <dedyhermawan076@gmail.com>
2021-11-11 20:38:23 +0700

    update new db

--- backoffice/config.php
+++ backoffice/config.php
@@ -2,7 +2,7 @@
 $hostname = "dash-tombo.bgskr-project.my.id";
 $database = "dash_tombo";
 $username = "dash_tombo";
-$password = "1sampaitombo";
+$password = "1sampaitomboati";
 
 $mysqli =  mysqli_connect($hostname, $username, $password, $database);
 $koneksi =  mysqli_connect($hostname, $username, $password, $database);
